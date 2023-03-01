d9e00b3c08ed23c4105f7fddaf1a08df2d3beba7
mkellner43 <matthew.kellner@outlook.com>
2023-02-28 18:31:24 -0600

    removed space

--- src/api/notifications.js
+++ src/api/notifications.js
@@ -52,7 +52,6 @@ export const getNotifications = (setToken, read=false) => {
   })
 }
 
-
 export const deleteNotification = (id, setToken, setNotifications) => {
   return fetch(`http://localhost:3000/api/v1/users/notifications/${id}`, {
     method: 'delete',
